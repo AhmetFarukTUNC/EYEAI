e6359f0a9a9ab88e19d2152c3305c204f1e94b30
AhmetFarukTUNC <atunc8402@gmail.com>
2026-09-02 17:52:31 +0300

    We devoloped this web application that detect eye disease from eye retina

---
+++ asp.netcorewebapi/asp.netcorewebapi/Services/AIService.cs
@@ -0,0 +1,104 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using EyeAI.API.Models;
+
+
+namespace EyeAI.API.Services
+{
+    public class AIService
+    {
+
+        private readonly HttpClient _client;
+
+
+        public AIService()
+        {
+            _client = new HttpClient();
+        }
+
+
+
+
+
+        public async Task<AIResult?> Predict(IFormFile image)
+        {
+
+            using var content = new MultipartFormDataContent();
+
+
+
+            using var stream = image.OpenReadStream();
+
+
+
+            var fileContent = new StreamContent(stream);
+
+
+
+            fileContent.Headers.ContentType =
+                new MediaTypeHeaderValue(
+                    image.ContentType
+                );
+
+
+
+            content.Add(
+                fileContent,
+                "image",
+                image.FileName
+            );
+
+
+
+
+
+            var response =
+                await _client.PostAsync(
+                    "http://127.0.0.1:5000/predict",
+                    content
+                );
+
+
+
+
+            var json =
+                await response.Content.ReadAsStringAsync();
+
+
+
+            // Flask cevabını görmemiz için
+            Console.WriteLine("FLASK RESPONSE:");
+            Console.WriteLine(json);
+
+
+
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(
+                    "AI API Error: " + json
+                );
+            }
+
+
+
+
+
+            var result =
+                JsonSerializer.Deserialize<AIResult>(
+                    json,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    }
+                );
+
+
+
+            return result;
+
+        }
+
+
+    }
+}
